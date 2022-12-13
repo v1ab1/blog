becffb2fe5ec7166832478635c4c5a20e8e0f49f
v1ab1 <sharkandmouse@gmail.com>
2022-12-14 00:56:33 +0500

    db added

--- server/index.js
+++ server/index.js
@@ -1,9 +1,31 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+
+mongoose
+    .connect('mongodb+srv://admin:8888@cluster0.5chjhac.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => console.log('DB is OK'))
+    .catch((err) => console.log('DB error', err));
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send("Server is fine!");
+});
+
+app.post('/auth/login', (req,res) => {
+    const token = jwt.sign(
+        {
+        email: req.body.email
+        },
+        "secretkey",
+    );
+    res.json({
+        success: true,
+        token
+    });
 });
 
 app.listen(4000, (err) => {
