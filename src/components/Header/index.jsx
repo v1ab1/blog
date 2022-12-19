f875be5d14f69879eed49ea3cee467914dfd5f18
v1ab1 <sharkandmouse@gmail.com>
2022-12-19 23:20:02 +0500

    bg changed

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,14 @@ import style from './Header.module.sass';
 export const Header = () => {
     return (
     <div className={style.Header}>
-        1
+      <div className={style.content}>
+          <div>
+            Главная
+          </div>
+          <div>
+            Вход
+          </div>
+      </div>
     </div>
   );
 }
