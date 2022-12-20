6cbed41140548f10c8c1d6a92a986bbe6b19b71c
v1ab1 <sharkandmouse@gmail.com>
2022-12-20 17:33:01 +0500

    work on main

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -3,6 +3,11 @@ import style from './Home.module.sass';
 export const Home = () => {
     return (
     <div className={style.Home}>
+      <div className={style.content}>
+        <p>
+          Home
+        </p>
+      </div>
     </div>
   );
 }
