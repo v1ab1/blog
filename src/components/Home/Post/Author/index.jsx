52aa289189b0155987511aade0f159d6cd55585d
v1ab1 <sharkandmouse@gmail.com>
2022-12-21 23:53:41 +0500

    author component added

--- src/components/Home/Post/Author/index.jsx
+++ src/components/Home/Post/Author/index.jsx
@@ -3,6 +3,15 @@ import style from './Author.module.sass';
 export const Author = () => {
     return (
     <div className={style.Author}>
+        <img src='' alt="" />
+        <div>
+            <p>
+                User
+            </p>
+            <span>
+                Date
+            </span>
+        </div>
     </div>
   );
 }
--- src/components/Home/Post/index.jsx
+++ src/components/Home/Post/index.jsx
@@ -1,9 +1,11 @@
+import { Author } from './Author';
 import style from './Post.module.sass';
 
 export const Post = () => {
     return (
     <div className={style.Post}>
       <img className={style.image} alt="" src=""/>
+      <Author />
     </div>
   );
 }
