3016e07fa2c2046fa81a97e9d502ca23dea1a3e0
v1ab1 <sharkandmouse@gmail.com>
2022-12-21 23:43:58 +0500

    work on post

--- src/components/Home/Post/index.jsx
+++ src/components/Home/Post/index.jsx
@@ -3,7 +3,7 @@ import style from './Post.module.sass';
 export const Post = () => {
     return (
     <div className={style.Post}>
-      1
+      <img className={style.image} alt="" src=""/>
     </div>
   );
 }
