5930ff8b8a16c13eb28ffed2ec34276b8c0f32d8
v1ab1 <sharkandmouse@gmail.com>
2023-01-02 00:11:17 +0500

    work on posts

---
+++ src/components/Home/Post/Tag/index.jsx
@@ -0,0 +1,11 @@
+import style from './Tag.module.sass';
+
+export const Tag = () => {
+    return (
+    <div className={style.Tag}>
+        <span>
+            Tag
+        </span>
+    </div>
+  );
+}
--- src/components/Home/Post/index.jsx
+++ src/components/Home/Post/index.jsx
@@ -1,4 +1,5 @@
 import { Author } from './Author';
+import { Tag } from './Tag';
 import style from './Post.module.sass';
 
 export const Post = () => {
@@ -7,8 +8,13 @@ export const Post = () => {
       <div className={style.postImage}></div>
       <div className={style.info}>
         <Author />
-        <div>
+        <div className={style.headerAndTags}>
           <h2>BLBLLBLBALBLBLABBLALBALBBALLBALBALLALBLALBLABLALBLAB</h2>
+          <div className={style.tags}>
+            <Tag />
+            <Tag />
+            <Tag />
+          </div>
         </div>
       </div>
     </div>
